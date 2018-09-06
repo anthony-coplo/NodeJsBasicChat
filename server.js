63134aac511820eef7c6648d155edfcf212c8f11
Anthony Coplo <anthony.coplo-isc.france@ibm.com>
2018-09-06 15:19:42 +0200

    basic chat

---
+++ server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html'); //read the html file
+});
+
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+});
+
+io.on('connection', function(socket){ //on connection event
+    socket.on('chat msg', function(msg){ //on chat message event 
+        if(msg != "" ){ //verify the message is not empty
+            io.emit('chat msg',msg) //finally broadcast the message
+        } 
+    });
+});
