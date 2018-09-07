285535d009a365f72a3a2a5eb595de444ef4e144
DylanLeblanc <42961896+DylanLeblanc@users.noreply.github.com>
2018-09-07 16:08:32 +0200

    without ordering list

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [];
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html'); //read the html file
 });
@@ -13,15 +15,30 @@ http.listen(8080, function(){
 io.on('connection', function(socket){ //on connection event
 
     var userConnected = null;
+    var alreadyConnect = false;
 
     socket.on('userConnection',function(user){
         userConnected = user;
+
+        users.push(userConnected);
         socket.broadcast.emit('chat msg', userConnected + " is connected");
-        io.emit('addUser', "- " + userConnected);
+
+        if(alreadyConnect == false){
+            for(var i = 0; i< users.length; i++){
+                io.emit('addUser', users[i]);
+            }
+            alreadyConnect = true;
+        }
+        else{
+            socket.broadcast.emit('addUser', user);
+        }
+
     })
 
     .on('disconnect',function(){
-        socket.broadcast.emit('chat msg', userConnected + " is disconnected");
+        if(userConnected !== undefined){
+            socket.broadcast.emit('chat msg', userConnected + " is disconnected");
+        }
     })
 
     .on('chat msg', function(msg){ //on chat message event 
