f79a0b0ec30ecbeae60825bdedf9179873e54453
Anthony Coplo <anthony.coplo-isc.france@ibm.com>
2018-09-07 10:41:31 +0200

    v1

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ http.listen(8080, function(){
 });
 
 io.on('connection', function(socket){ //on connection event
+    socket.broadcast.emit('newUser',"a new user is connected");
+    socket.on('disconnect',function(){
+        socket.broadcast.emit('disconnectUser',"a user is disconnected");
+    })
     socket.on('chat msg', function(msg){ //on chat message event 
         if(msg != "" ){ //verify the message is not empty
-            io.emit('chat msg',msg) //finally broadcast the message
+            io.emit('chat msg',msg); //finally broadcast the message
         } 
     });
 });
